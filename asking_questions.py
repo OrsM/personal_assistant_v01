6166156dcab8c98a56a5860d138faae29729abfd
Miguel Ors <miguelors@gmail.com>
2022-12-06 18:06:32 +0100

    updated to simplify input/output of text files

--- asking_questions.py
+++ asking_questions.py
@@ -11,19 +11,23 @@ ORGANIZATION=os.getenv('ORGANIZATION')
 openai.organization = ORGANIZATION
 openai.api_key = API_KEY
 
-sample_input = open('examples/input_02.txt', 'r', encoding="utf-8").read()
-basic_prompt = "Can you summarize the following text into the key points and next steps?\n"
-full_prompt = "%s %s"%(basic_prompt,sample_input)
+def summarize_notes(input_filename = "input_01.txt"):
+    """Provide a summary with key points & next steps for an input of raw text"""
+    input_file = open("%s%s"%('examples/', input_filename), 'r', encoding="utf-8").read()
+    basic_prompt = "Can you summarize the following text into the key points and next steps?\n"
+    full_prompt = "%s %s"%(basic_prompt,input_file)
 
-output = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=full_prompt,
-    max_tokens=250,
-    temperature=1
-)
+    output = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=full_prompt,
+        max_tokens=250,
+        temperature=1
+    )
 
-filename = "%s%s%s%s"%('examples/',datetime.datetime.now().strftime("%Y%m%d-%H%M"),'_output','.txt')
+    output_filename = "%s%s%s%s"%('examples/',datetime.datetime.now().strftime("%Y%m%d-%H%M"),'_output','.txt')
 
-result = open(filename,'w')
-result.write(output['choices'][0]['text'])
-result.close()
+    output_file = open(output_filename,'w')
+    output_file.write(output['choices'][0]['text'])
+    output_file.close()
+
+summarize_notes()
