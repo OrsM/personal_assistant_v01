41bd8b7af73876377e42aecb45dd372092bf67a8
Miguel Ors <miguelors@gmail.com>
2022-12-06 17:50:38 +0100

    modified code to clarify the question to the AI

--- asking_questions.py
+++ asking_questions.py
@@ -3,7 +3,7 @@ import openai
 import datetime
 from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 API_KEY=os.getenv('API_KEY')
 ORGANIZATION=os.getenv('ORGANIZATION')
@@ -11,18 +11,18 @@ ORGANIZATION=os.getenv('ORGANIZATION')
 openai.organization = ORGANIZATION
 openai.api_key = API_KEY
 
-sample_input = open('sample_input_01.txt', 'r', encoding="utf-8").read()
-basic_prompt = "can you summarize the following text into key bullets and next steps?\n"
+sample_input = open('examples/input_02.txt', 'r', encoding="utf-8").read()
+basic_prompt = "Can you summarize the following text into the key points and next steps?\n"
 full_prompt = "%s %s"%(basic_prompt,sample_input)
 
 output = openai.Completion.create(
     model="text-davinci-003",
     prompt=full_prompt,
-    max_tokens=200,
+    max_tokens=250,
     temperature=1
 )
 
-filename = "%s%s%s"%(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"),'_summary','.txt')
+filename = "%s%s%s%s"%('examples/',datetime.datetime.now().strftime("%Y%m%d-%H%M"),'_output','.txt')
 
 result = open(filename,'w')
 result.write(output['choices'][0]['text'])
