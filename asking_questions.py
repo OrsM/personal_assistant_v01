2ee1c55144ec9ef086d46d899df17b14e507e5df
Miguel Ors <OrsM@users.noreply.github.com>
2023-02-27 09:28:03 +0100

    adds a new function to directly ping the api

--- asking_questions.py
+++ asking_questions.py
@@ -12,6 +12,7 @@ ORGANIZATION=os.getenv('ORGANIZATION')
 openai.organization = ORGANIZATION
 openai.api_key = API_KEY
 
+
 def summarize_notes(input_filename = "input_01.txt"):
     """Provide a summary with key points & next steps for an input of raw text"""
     input_file = open("%s%s"%('examples/', input_filename), 'r', encoding="utf-8").read()
@@ -31,4 +32,14 @@ def summarize_notes(input_filename = "input_01.txt"):
     output_file.write(output['choices'][0]['text'])
     output_file.close()
 
-summarize_notes()
+# summarize_notes()
+
+def ask_ai(question = "how are you doing today?"):
+    """ask Open AI a question, get an answer"""
+    output = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=question,
+        max_tokens=250,
+        temperature=1
+    )
+    return output['choices'][0]['text']
